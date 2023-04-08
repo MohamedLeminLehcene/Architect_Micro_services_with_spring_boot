aa27e4f028d1487a6da25f0693c029b28330b88e
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-04-08 18:25:28 +0000

    Prémiere commit

---
+++ Architectures-Micro-services/src/test/java/com/sdia/ArchitecturesMicroservices/ArchitecturesMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdia.ArchitecturesMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturesMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
